df6cefa196f07757fb999864a8cdb6e38676f62c
Taylor Sarrafian <surprisetalk@gmail.com>
2016-05-29 22:21:39 +0000

    create thing in pile

--- pile.js
+++ pile.js
@@ -1,7 +1,9 @@
 
 var piles = require('./piles.js');
 
-module.exports = ( name ) => 
+// TODO: errors
+module.exports = name => 
 ({
+    create: data => ( new piles[ name ]( data ) ).save( ( err, thing ) => err ? Promise.reject( err ) : thing ),
     fetch: piles[ name ].find
 });
